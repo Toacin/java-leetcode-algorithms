2df8d9321b832f1273985d113408c2915ee4d2a9
Toacin <toacinp@outlook.com>
2023-01-17 17:12:37 -0700

    "superdigit solved using long"

--- src/main/java/HackerRank/superDigit.java
+++ src/main/java/HackerRank/superDigit.java
@@ -4,11 +4,18 @@ public class superDigit {
     public static int superDigit(String n, int k) {
         // Write your code here
         String numberStringified = n;
-
+        boolean firstIteration = true;
         while(numberStringified.length()>1) {
-            int runningTotal = 0;
+            long runningTotal = 0;
             for (int i=0; i<numberStringified.length(); i++) {
+                System.out.println("the number being added to runnning total is: " + Integer.parseInt(String.valueOf(numberStringified.charAt(i))));
                 runningTotal += Integer.parseInt(String.valueOf(numberStringified.charAt(i)));
+            }
+            System.out.println("the running total is: " + runningTotal);
+            if (firstIteration) {
+                runningTotal *= k;
+                firstIteration = false;
+                System.out.println("the running total updated with k is: " + runningTotal);
             }
             numberStringified = String.valueOf(runningTotal);
         }
