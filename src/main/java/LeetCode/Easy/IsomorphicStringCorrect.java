9f7c873ac83e8c26953856dc4d75f69798d365dc
Toacin <toacinp@outlook.com>
2023-02-18 19:21:55 -0700

    isomorphic string more optimized solution found

--- src/main/java/LeetCode/Easy/IsomorphicStringCorrect.java
+++ src/main/java/LeetCode/Easy/IsomorphicStringCorrect.java
@@ -7,16 +7,13 @@ public class IsomorphicStringCorrect {
     public boolean isIsomorphic(String s, String t) {
         if (s.length() != t.length()) return false;
         HashMap<String, String> stringMap = new HashMap<>();
-        HashMap<String, String> reverseStringMap = new HashMap<>();
         for (int i=0; i<s.length(); i++) {
             String firstStringChar = String.valueOf(s.charAt(i));
             String secondStringChar = String.valueOf(t.charAt(i));
 
-            if (stringMap.get(firstStringChar) == null) stringMap.put(firstStringChar, secondStringChar);
+            if (stringMap.get(firstStringChar) == null && stringMap.containsValue((secondStringChar))) return false;
+            else if (stringMap.get(firstStringChar) == null) stringMap.put(firstStringChar, secondStringChar);
             else if (!stringMap.get(firstStringChar).equals(secondStringChar)) return false;
-
-            if (reverseStringMap.get(secondStringChar) == null) reverseStringMap.put(secondStringChar, firstStringChar);
-            else if (!reverseStringMap.get(secondStringChar).equals(firstStringChar)) return false;
         }
         return true;
     }
